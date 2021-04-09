714b8108b1bb9e9936c2ca86e3a8f59bb0c735f5
Laura Manor <>
2021-04-09 18:54:15 +0000

    format update

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-
 class Feline():
     family = "Felidae"
+
     def __init__(self, name, bff):
         self.name = name
         self.best_friend = bff
 
     # def description(self):
     #     return self.name + "'s best friend is" + self.best_friend
-    
+
+
 class Lion(Feline):
     species = "P. leo"
+
 
 class Tiger(Feline):
     species = "P. tigris"
 
+
 class Cat(Feline):
     species = "F. catus"
+
 
 hobbes = Feline("Hobbes", "Calvin")
 
